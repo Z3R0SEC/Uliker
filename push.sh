ee1821993466f2e24f2cce7f3910df41585e443b
Z3R0 <144493047+Z3R0SEC@users.noreply.github.com>
2025-06-16 19:28:02 +0200

    Update and rename push.sh to requirements.txt

--- push.sh
+++
@@ -1,7 +0,0 @@
-echo "# Uliker" >> README.md
-git init
-git add .
-git commit -m "Supper Fast FB Boosta"
-git branch -M main
-git remote add origin https://github.com/Z3R0SEC/Uliker.git
-git push -u origin main
